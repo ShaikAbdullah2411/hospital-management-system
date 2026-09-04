aeb579362c6f3cb18659939baaaeaac5d6c3b930
shaik Abdullah <shaikabdullahsa2411@gmail.com>
2026-09-04 17:38:45 +0530

    added PatientService CI pipeline

--- PatientService/PatientService/src/test/java/com/hospitalmanagement/PatientService/PatientServiceApplicationTests.java
+++ PatientService/PatientService/src/test/java/com/hospitalmanagement/PatientService/PatientServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.hospitalmanagement.PatientService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PatientServiceApplicationTests {
 
 	@Test
